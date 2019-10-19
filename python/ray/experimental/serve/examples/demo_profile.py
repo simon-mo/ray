ab5832a5851c2ae7111f35828bbfe549decbbbaa
Simon Mo <xmo@berkeley.edu>
2019-10-18 21:20:16 -0700

    Add readme

--- python/ray/experimental/serve/examples/demo_profile.py
+++ python/ray/experimental/serve/examples/demo_profile.py
@@ -1,5 +1,19 @@
 """
-Example service that prints out http context.
+Let this file run.
+And in a new terminal, run ApacheBench
+   ab -c 20 -n 60 http://127.0.0.1:8000/sleep
+
+Ploting code
+
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_json('/tmp/serve_profile.jsonl', lines=True)
+for i, row in df.iterrows():
+    plt.plot([row['start'], row['end']], [i,i])
+plt.xlabel("UNIX Timestamp")
+plt.ylabel("Query ID")
+plt.title("Handling ab -c 20 -n 60 http://127.0.0.1:8000/sleep")
+plt.savefig("profile.png")
 """
 
 import time
